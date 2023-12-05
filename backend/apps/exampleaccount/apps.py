5d0efb303dd2a8b5612a9aa64974c04e5ba0c2a7
Bishow Thapa <bishow.thapa@info.com.np>
2023-12-05 16:12:55 +0545

    fixed roots app urls in the name of the aps.py file of app

--- backend/apps/exampleaccount/apps.py
+++ backend/apps/exampleaccount/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ExampleaccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'exampleaccount'
+    name = 'apps.exampleaccount'
